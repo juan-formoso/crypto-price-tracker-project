18acd9554994f08b09547db7d197dcf98a4c4762
Juan Formoso <trajano.juan@gmail.com>
2022-01-05 16:33:17 -0300

    Refatorando Coin.js

--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-const Coin = ({ name, image, symbol, price, volume }) => {
+const Coin = ({
+  name,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  image,
+  priceChange,
+}) => {
   return (
     <div className="coin-container">
       <div className="coin-row">
@@ -12,8 +20,20 @@ const Coin = ({ name, image, symbol, price, volume }) => {
         <div className="coin-data">
           <p className="coin-price">${price}</p>
           <p className="coin-volume">${volume.toLocaleString()}</p>
+
+          {priceChange < 0 ? (
+            <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+          ) : (
+            <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+          )}
+
+          <p className="coin-marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
         </div>
       </div>
     </div>
   );
 };
+
+export default Coin;
